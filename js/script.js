f9d4bd32d84448bcfa5c42c16f4982091a8d6b5f
legionY <kuzprodev.com>
2020-11-08 22:59:31 +0200

    ifelse

--- js/script.js
+++ js/script.js
@@ -21,19 +21,48 @@
     }
 
 Проверить, чтобы все работало без ошибок в консоли */
- const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
- const personalMovieDB = {
-     count: numberOfFilms,
-     movies: {},
-     actors: {},
-     genres: [],
-     privat: false
- };
- const a = prompt('Один из последних просмотренныхфильмов?', ''),
- b =prompt('На сколько оценете его?', '10.0'),
- c= prompt('Один из последних просмотренныхфильмов?', ''),
- d =prompt('На сколько оценете его?', '10.0');
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренныхфильмов?', ''),
+    b = prompt('На сколько оценете его?', '10.0'),
+    c = prompt('Один из последних просмотренныхфильмов?', ''),
+    d = prompt('На сколько оценете его?', '10.0');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+// const num = 50;
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log("over big!");
+// } else {
+//     console.log('OK!');
+// }
+
+// (num === 50) ? console.log('OK!'): console.log('Error!');
+const num = 50;
+switch (num) {
+    case 49: //case работет только на строгое сравнение
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 50: //case '50' проверка строчки
+        console.log('In dot!');
+        break;
+    default:
+        console.log("не в этот раз");
+}
