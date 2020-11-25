3b2d1564562901c131c31a298a95045103937f23
legionY <kuzprodev.com>
2020-11-25 13:56:23 +0200

    callback

--- js/script.js
+++ js/script.js
@@ -1,81 +1,25 @@
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
 'use strict';
-let numberOfFilm;
-
-function start() {
-    numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilm == '' || numberOfFilm == null || isNaN(numberOfFilm)) {
-        //если  не число то возвращает правду, если нет то ложь isNaN(numberOfFilm)
-        numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-    // если не одно из условий не будет выполненно то вопрос будет повторяться -цикл while
+//callback function функция которая должна выполниться после того как другая завершила своё выполнение
+function first() {
+    //Do something
+    setTimeout(function(){
+        console.log(1);
+    },500);
 }
-start();
-const personalMovieDB = {
-    count: numberOfFilm,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+function second(){
+    console.log(2);
 }
-rememberMyFilms();
+first();
+second();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотренно довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы класический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
+function learnJS(lang, callback){
+console.log(`Я учу: ${lang}`);
+callback();
 }
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    // if (personalMovieDB.privat == false) {
-    if (!hidden) { //если база данных не скрыто то выполняется !hidden=false hidden=true
-        console.log(personalMovieDB);
-    }
+function done(){
+    console.log('Я прошел этот урок!');
 }
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        // const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-        // personalMovieDB.genres[i - 1] = genre;
-        // console.log(genre);
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
-    }
-}
-writeYourGenres();
-
-// Код возьмите из предыдущего домашнего задания
+learnJS('JavaScript', done);
+// learnJS('JavaScript', function(){
+//     console.log('Я выполнил урок');
+// });
